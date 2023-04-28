29a79913b09ca8f0b611775c4cb0bb2a688fa8db
snehashislenka <lenka.s@northeastern.edu>
2023-04-28 13:00:19 -0400

    Upload reviews UI

--- Streamlit/Pages/Upload Reviews.py
+++ Streamlit/Pages/Upload Reviews.py
@@ -4,9 +4,13 @@ from google.cloud import storage
 from google.oauth2 import service_account
 import tempfile
 import os
+import requests
 
+# Define the API endpoint URL
+URL = 'http://bigdata7245-finalproject.ue.r.appspot.com'
 
-with st.form("Upload_form", clear_on_submit=True):
+def uploadReviewsPage():
+    with st.form("Upload_form", clear_on_submit=True):
 
         uploaded_file=st.file_uploader("Choose an review file",type='csv',accept_multiple_files=False)
         submitted = st.form_submit_button("Upload File")
@@ -26,7 +30,12 @@ with st.form("Upload_form", clear_on_submit=True):
                     os.fsync(tmp_file.fileno())
                     
                     ##Request fastapi for the resturant_id
-                    restaurant_id="Random_string"
+                    restaurant_id=""
+                    headers = {"Authorization": "Bearer " + st.session_state['login_token']}
+                    res = requests.get(URL + '/get_restaurant_id', headers=headers)
+                    if res.status_code == 200:
+                         restaurant_id = res.json()
+                    
                     
                     # Upload the temporary file to Cloud Storage
                     blob = bucket.blob(f"raw_reviews/{uploaded_file.name}-id-{restaurant_id}")
@@ -38,3 +47,10 @@ with st.form("Upload_form", clear_on_submit=True):
                 st.session_state.upload_success = True
             elif submitted and uploaded_file is None:
                 st.warning('Select a file to upload')
+
+
+if __name__ == "__main__":
+    if st.session_state['login_success']:
+        uploadReviewsPage()
+    else:
+        st.write("Login to access this page!")
