4d563bcf1d64c2bfdcd785b216ea68056e5f30cc
anujkumar98 <kumar.anuj@northeastern.edu>
2023-04-28 13:25:29 -0400

    name change

--- Streamlit/home.py
+++ Streamlit/home.py
@@ -4,6 +4,6 @@ st.set_page_config(
     page_title = "Big Data Systems and Intelligence Analytics: Final Project"
 )
 
-st.title("Big Data Systems and Intelligence Analytics")
+st.title("Big Data Systems and Intelligence Analytics DAMG 7245")
 st.header("Final Project")
 st.sidebar.success("Select a page above.")
